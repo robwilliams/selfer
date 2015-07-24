b997c667438772cd2a5827429bd9a443ba070813
Robert Williams <rob@r-williams.com>
2015-07-25 00:14:22 +0100

    Allow multiple methods to be delegated in one go.

--- lib/selfer.rb
+++ lib/selfer.rb
@@ -1,12 +1,14 @@
 require "selfer/version"
 
 module Selfer
-  def selfer(method)
-    define_singleton_method(method) do |*args|
-      instance = allocate
-      init_args = args.shift(instance.method(:initialize).arity)
-      instance.send(:initialize, *init_args)
-      instance.public_send(method, *args)
+  def selfer(*method_names)
+    method_names.each do |method_name|
+      define_singleton_method(method_name) do |*args|
+        instance = allocate
+        init_args = args.shift(instance.method(:initialize).arity)
+        instance.send(:initialize, *init_args)
+        instance.public_send(method_name, *args)
+      end
     end
   end
 end
--- test/selfer_test.rb
+++ test/selfer_test.rb
@@ -5,6 +5,7 @@ class TheClass
 
   selfer :do_thing
   selfer :arity_test
+  selfer :one, :two
 
   def initialize(arg1,arg2,arg3)
     @initarg1 = arg1
@@ -14,6 +15,14 @@ class TheClass
 
   def do_thing
     [@initarg1, @initarg2, @initarg3]
+  end
+
+  def one
+    :one
+  end
+
+  def two
+    :two
   end
 
   def arity_test(methodarg4)
@@ -27,11 +36,17 @@ class SelferTest < Minitest::Test
   end
 
   def test_it_delegates_to_the_instance
-    assert_equal TheClass.do_thing(:arg1, :arg2, :arg3), [:arg1,:arg2,:arg3]
+    assert_equal [:arg1,:arg2,:arg3],
+                 TheClass.do_thing(:arg1, :arg2, :arg3)
   end
 
   def test_it_delegates_to_the_instance_with_arity
     assert_equal [:arg1,:arg2,:arg3, :arg4],
                  TheClass.arity_test(:arg1, :arg2, :arg3, :arg4)
+  end
+
+  def test_it_can_delegate_multiple_methods
+    assert_equal :one, TheClass.one(1,2,3)
+    assert_equal :two, TheClass.two(1,2,3)
   end
 end
